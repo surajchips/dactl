ed4540d5a7dd1e722aabbea09e207bc80753ed1a
Mike Nguyen <mikenguyen7268@gmail.com>
2023-05-31 17:45:24 -0500

    added minkowski sum and halfplane intersection

---
+++ content/geometry/HalfplaneIntersection.h
@@ -0,0 +1,77 @@
+/**
+ * Author: Mike Nguyen
+ * Date: 2023-05-31
+ * Source: MIT ICPC Notebook
+ * Description: Compute intersection of a set of halfplanes. A point is in a halfplane if it lies on the left of the plane boundary. 
+ * Status: tested on kattis:birthdaycake
+*/
+#include "../geometry/Point.h"
+
+using P = Point<double>;
+constexpr EPS = 1e-10;
+
+int lineIntersection(const P& s1, const P& e1, const P& s2, const P& e2, P& r) {
+	if ((e1-s1).cross(e2-s2)) { //if not parallell
+		r = s2-(e2-s2)*(e1-s1).cross(s2-s1)/(e1-s1).cross(e2-s2);
+		return 1;
+	} else {
+		return -((e1-s1).cross(s2-s1)==0 || s2==e2);
+    }
+}
+
+struct HalfPlane {
+    P p1, p2;
+    explicit HalfPlane(P a = P(), P b = P()) : p1(a), p2(b) {}
+    P intpo(HalfPlane y) {
+        P r; 
+        assert(lineIntersection(p1, p2, y.p1, y.p2, r) == 1);
+        return r;
+    }
+    P dir() { return p2-p1; }
+    bool contains(P x) { return (p2-p1).cross(x-p1) < EPS; }
+    bool out(P x) { return !contains(x); }
+};
+
+vector<P> halfPlaneIntersection(vector<HalfPlane> b) {
+    sort(begin(b), end(b), [](HalfPlane al, HalfPlane bl) {
+        auto a = al.dir(), bb = bl.dir();
+        if (a.x * bb.x < 0) return a.x < 0;
+        if (abs(a.x) < EPS) {
+            if (abs(bb.x) < EPS) return a.y > 0 && bb.y < 0;
+            if (bb.x < 0) return a.y > 0;
+            if (bb.x > 0) return true;
+        } 
+        if (abs(bb.x) < EPS) {
+            if (a.x < 0) return bb.y < 0;
+            if (a.x > 0) return false;
+        }
+        return a.cross(bb) > 0;
+    });
+    int n = sz(b);
+    int q = 1, h = 0, i;
+    vector<HalfPlane> c(sz(b) + 10);
+    for (i=0; i<n; i++) {
+        while (q < h && b[i].out(c[h].intpo(c[h-1]))) h--;
+        while (q < h && b[i].out(c[q].intpo(c[q+1]))) q++;
+        c[++h] = b[i];
+        if (q < h && abs(c[h].dir().cross(c[h-1].dir())) < EPS) {
+            if (c[h].dir().dot(c[h-1].dir()) > 0) {
+                h--;
+                if (b[i].out(c[h].p1)) c[h] = b[i];
+            } else {
+                return {};
+            }
+        }
+    }
+    while (q < h-1 && c[q].out(c[h].intpo(c[h-1]))) h--;
+    while (q < h-1 && c[h].out(c[q].intpo(c[q+1]))) q++;
+    if (h-q <= 1) {
+        return {};
+    }c[h+1] = c[q];
+    vector<P> s;
+    for (i = q; i<=h; i++) {
+        s.push_back(c[i].intpo(c[i+1]));
+    }
+    s.push_back(s[0]);
+    return s;
+} 
---
+++ content/geometry/Minkowski.h
@@ -0,0 +1,37 @@
+/**
+ * Author: Mike Nguyen
+ * Date: 2023-05-31
+ * Source: CP-Algorithms
+ * Description: Compute Minkowski sum of 2 convex polygons. 
+ * Status: tested
+*/
+#include "../geometry/Point.h"
+
+using P = Point<ll>;
+
+void rotate_polygon(vector<P>& poly) {
+    size_t pos = 0;
+    for(size_t i = 1; i < poly.size(); i++){
+        if(poly[i].y < poly[pos].y || (poly[i].y == poly[pos].y && poly[i].x < poly[pos].x))
+            pos = i;
+    }
+    rotate(begin(poly), begin(poly)+pos, end(poly));
+}
+
+vector<P> minkowski(vector<P> poly1, vector<P> poly2) {
+    rotate_polygon(poly1); rotate_polygon(poly2);
+    size_t n = poly1.size(), m = poly2.size();
+    poly1.push_back(poly1[0]);
+    poly1.push_back(poly1[1]);
+    poly2.push_back(poly2[0]);
+    poly2.push_back(poly2[1]);
+    vector<P> ret;
+    size_t i = 0, j = 0;
+    while (i < n || j < m) {
+        ret.push_back(poly1[i] + poly2[j]);
+        auto c = (poly1[i+1] - poly1[i]).cross(poly2[j+1]-poly2[j]);
+        i += c >= 0;
+        j += c <= 0;
+    }
+    return ret;
+}
