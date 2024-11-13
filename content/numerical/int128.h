67ff732fe8dd1cfd48367a4370c8004c1b1a9951
surajchips <59129788+surajchips@users.noreply.github.com>
2024-11-13 15:30:39 -0600

    Create int128.h

---
+++ content/numerical/int128.h
@@ -0,0 +1,30 @@
+/**
+ * Author: DrinkMoreBoilingWater
+ * Date: 2019
+ * Source: https://codeforces.com/blog/entry/75044?
+ * Description: Operations for int128 integers
+ */
+#pragma once
+
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+bool cmp(__int128 x, __int128 y) { return x > y; }
